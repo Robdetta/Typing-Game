4c829d1d11f73bb63ff0ef5558e56403d7622a44
robbie <robdetta@gmail.com>
2022-01-17 00:25:21 -0800

    hopefully fixed

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express();
 
 app.use(express.static("Typing-Game"));
 
+app.get("/", function (req, res) {
+  res.send("Get the homepage");
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
