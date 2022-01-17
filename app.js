6e0872bef8d8ea1c1617499d9c245d1c88c09edc
robbie <robdetta@gmail.com>
2022-01-17 00:29:16 -0800

    did I get it this time

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("Typing-Game"));
 
 app.get("/", function (req, res) {
-  res.send("Get the homepage");
+  res.sendFile("index.html");
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
